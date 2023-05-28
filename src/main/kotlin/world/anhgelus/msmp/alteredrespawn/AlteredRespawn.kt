4022fc776d77c620ab98a7b49ad6e31455db464d
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@gmx.com>
2023-05-28 22:57:51 +0200

    Initial commit

---
+++ src/main/kotlin/world/anhgelus/msmp/alteredrespawn/AlteredRespawn.kt
@@ -0,0 +1,13 @@
+package world.anhgelus.msmp.alteredrespawn
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class AlteredRespawn: JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
