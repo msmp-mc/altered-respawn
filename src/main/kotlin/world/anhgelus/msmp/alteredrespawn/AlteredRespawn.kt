7c82476cfea66f2b6e2c08121d28529221102f99
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@gmx.com>
2023-05-28 23:02:05 +0200

    feat(base) utility methods of main file

--- src/main/kotlin/world/anhgelus/msmp/alteredrespawn/AlteredRespawn.kt
+++ src/main/kotlin/world/anhgelus/msmp/alteredrespawn/AlteredRespawn.kt
@@ -1,13 +1,25 @@
 package world.anhgelus.msmp.alteredrespawn
 
 import org.bukkit.plugin.java.JavaPlugin
+import java.util.logging.Logger
 
 class AlteredRespawn: JavaPlugin() {
     override fun onEnable() {
-        // Plugin startup logic
+        LOGGER = logger
+        LOGGER.info("AlteredRespawn has been enabled!")
     }
 
     override fun onDisable() {
-        // Plugin shutdown logic
+        LOGGER.info("AlteredRespawn has been disabled!")
+    }
+
+    companion object {
+        lateinit var INSTANCE: AlteredRespawn
+            private set
+        lateinit var LOGGER: Logger
+
+        fun getInstance(): AlteredRespawn {
+            return INSTANCE
+        }
     }
 }
