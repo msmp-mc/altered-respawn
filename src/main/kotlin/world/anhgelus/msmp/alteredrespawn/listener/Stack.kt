d3d69d2e131f2d664d41a24494b0634b175a8109
Akik4 <dupontstef80@gmail.com>
2023-06-07 22:16:33 +0200

    refactor(StatePlayer): changed by Mplayer

--- src/main/kotlin/world/anhgelus/msmp/alteredrespawn/listener/Stack.kt
+++ src/main/kotlin/world/anhgelus/msmp/alteredrespawn/listener/Stack.kt
@@ -9,6 +9,8 @@ import org.bukkit.event.player.PlayerInteractAtEntityEvent
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.ItemMeta
 import world.anhgelus.msmp.alteredrespawn.DefaultCondition
+import world.anhgelus.msmp.msmpcore.player.MPlayer
+import world.anhgelus.msmp.msmpcore.player.MPlayerManager
 import world.anhgelus.msmp.msmpcore.utils.ChatHelper
 
 class Stack {
@@ -23,30 +25,31 @@ class Stack {
 
         val item = ItemStack(Material.STRUCTURE_VOID)
         val itemMeta: ItemMeta = item.itemMeta!!
-
-        itemMeta.setDisplayName("§d" + player.name + "'s PILE")
+        itemMeta.setDisplayName("§d" + player.name + "'s STACK")
         item.setItemMeta(itemMeta)
 
-        ChatHelper.send(player.name + " is dead!")
-        player.sendHurtAnimation(5F)
-        event.isCancelled = true
-        player.health = 20.0
-        player.canPickupItems = false
-        var n: Int = 0
+        val mplayer = MPlayerManager.get(player)
+        mplayer.died(event) { mplayer, event ->
+            player.sendHurtAnimation(5F)
+            event.isCancelled = true
+            player.health = 20.0
+            player.canPickupItems = false
+            var n: Int = 0
 
-        for (i in 0 until player.inventory.size) {
-            val item = player.inventory.contents.get(n)
-            if (item != null) {
-                player.world.dropItemNaturally(player.location, item)
+            for (i in 0 until player.inventory.size) {
+                val item = player.inventory.contents[n]
+                if (item != null) {
+                    player.world.dropItemNaturally(player.location, item)
+                }
             }
-        }
 
-        player.world.dropItem(player.location, item)
-        player.inventory.clear()
-        StatePlayer().addDeath(player)
+            player.world.dropItem(player.location, item)
+            player.inventory.clear()
+        }
     }
     fun respawn(event: PlayerInteractAtEntityEvent){
         val player = event.player as Player
+        val mplayer = MPlayerManager.get(player)
 
         if (player.inventory.itemInMainHand.type != Material.STRUCTURE_VOID) {
             return
@@ -55,11 +58,15 @@ class Stack {
             if(!player.inventory.itemInMainHand.itemMeta!!.displayName.contains(it.displayName)){
                 return
             }
-            if(!StatePlayer().isDead(it)){
+            if(mplayer.isAlive()){
                 ChatHelper.sendInfoToPlayer(player, "This player is not dead!")
+                return@forEach
             }
-            it.teleport(event.rightClicked.location)
-            DefaultCondition().player(it)
+            mplayer.gainANewLife {
+                it.player.teleport(event.rightClicked.location)
+                DefaultCondition().player(it.player)
+            }
+
         }
     }
 }
