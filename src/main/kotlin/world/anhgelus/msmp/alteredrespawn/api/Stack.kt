9bf968233e1ad0bcc659911c1f8b0b86291f1a67
Akik4 <dupontstef80@gmail.com>
2023-06-11 00:07:33 +0200

    New(Respawn) : Adding respawning tiers

--- src/main/kotlin/world/anhgelus/msmp/alteredrespawn/api/Stack.kt
+++ src/main/kotlin/world/anhgelus/msmp/alteredrespawn/api/Stack.kt
@@ -1,9 +1,8 @@
 package world.anhgelus.msmp.alteredrespawn.api
 
-import org.bukkit.Bukkit
-import org.bukkit.GameMode
-import org.bukkit.Material
-import org.bukkit.Sound
+import org.bukkit.*
+import org.bukkit.attribute.Attribute
+import org.bukkit.entity.ArmorStand
 import org.bukkit.entity.EntityType
 import org.bukkit.entity.Player
 import org.bukkit.event.block.BlockPlaceEvent
@@ -11,7 +10,13 @@ import org.bukkit.event.entity.EntityDamageEvent
 import org.bukkit.event.player.PlayerInteractAtEntityEvent
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.ItemMeta
+import org.bukkit.inventory.meta.LeatherArmorMeta
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
+import org.bukkit.potion.PotionType
 import world.anhgelus.msmp.alteredrespawn.AlteredRespawn
+import world.anhgelus.msmp.msmpcore.MSMPCore
+import world.anhgelus.msmp.msmpcore.player.MPlayer
 import world.anhgelus.msmp.msmpcore.player.MPlayerManager
 import world.anhgelus.msmp.msmpcore.utils.ChatHelper
 
@@ -88,7 +93,9 @@ object Stack {
         } else {
             item.amount--
         }
-
+        if((event.rightClicked as ArmorStand).equipment?.helmet?.type != Material.LEATHER_HELMET) return
+        val helmet = ((event.rightClicked as ArmorStand).equipment?.helmet?.itemMeta as LeatherArmorMeta)
+        event.rightClicked.remove()
         var timer = 4
         for (i in 0..timer) {
             Bukkit.getScheduler().runTaskLater(AlteredRespawn.INSTANCE, Runnable {
@@ -101,13 +108,47 @@ object Stack {
                         respawned.sendTitle("respawned !", null, 0, 20, 0)
                         mplayer.toDefaultCondition()
                         respawned.world.strikeLightningEffect(event.rightClicked.location)
+                        respawned.addPotionEffect(PotionEffectType.BLINDNESS.createEffect(80, 5))
+                        respawned.addPotionEffect(PotionEffectType.SLOW.createEffect(160, 10))
+                        respawned.addPotionEffect(PotionEffectType.WEAKNESS.createEffect(160, 5))
+                        respawned.addPotionEffect(PotionEffectType.CONFUSION.createEffect(160, 1))
+                        this.setTier(respawned, helmet.color.asARGB())
                     }
                 }
                 timer -= 1
                 respawned.playSound(respawned, Sound.BLOCK_NOTE_BLOCK_BELL, 5F, i.toFloat())
             }, (i/0.05-20).toLong())
         }
+
+
     }
-
+    fun setTier(player: Player, helmet: Int){
+        when(helmet){
+            DyeColor.WHITE.color.asARGB() -> {
+                player.getAttribute(Attribute.GENERIC_MAX_HEALTH)?.baseValue = 15.0
+                player.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED)?.baseValue = 0.1
+                player.getAttribute(Attribute.GENERIC_ARMOR)?.baseValue = 0.0
+            }
+            DyeColor.GRAY.color.asARGB() -> {
+                player.getAttribute(Attribute.GENERIC_MAX_HEALTH)?.baseValue = 12.0
+                player.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED)?.baseValue = 0.2
+                player.addPotionEffect(PotionEffectType.ABSORPTION.createEffect(Int.MAX_VALUE, 5))
+            }
+            DyeColor.GREEN.color.asARGB() -> {
+                player.getAttribute(Attribute.GENERIC_MAX_HEALTH)?.baseValue = 10.0
+                player.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED)?.baseValue = 0.3
+                player.addPotionEffect(PotionEffectType.ABSORPTION.createEffect(Int.MAX_VALUE, 10))
+            }
+            DyeColor.BLUE.color.asARGB() -> {
+                player.getAttribute(Attribute.GENERIC_MAX_HEALTH)?.baseValue = 8.0
+                player.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED)?.baseValue = 0.4
+                player.addPotionEffect(PotionEffectType.ABSORPTION.createEffect(Int.MAX_VALUE, 15))
+            }
+            else-> {
+                player.getAttribute(Attribute.GENERIC_MAX_HEALTH)?.baseValue = 6.0
+                player.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED)?.baseValue = 0.5
+                player.addPotionEffect(PotionEffectType.ABSORPTION.createEffect(Int.MAX_VALUE, 20))
+            }
+        }
+    }
 }
-
